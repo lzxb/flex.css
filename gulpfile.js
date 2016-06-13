10d9d5684ae11743b63e0f0f38661b62ae067612
lzxb <1340641314@qq.com>
2016-06-13 14:54:38 +0800

    min.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
-
 var gulp = require('gulp');
 var less = require('gulp-less');
 var autoprefixer = require('gulp-autoprefixer');
 var cssmin = require('gulp-minify-css');
+var rename = require("gulp-rename");
 
 /**
- * 定义less编译任务
+ * 定义less编译任务，命令：gulp less
  */
 gulp.task('less', function () {
     gulp.src(['src/flex.less', 'src/data-flex.less']) //需要编译的less文件
@@ -14,11 +14,14 @@ gulp.task('less', function () {
             browsers: '> 0.1%'
         }))
         .pipe(cssmin()) //压缩css
-        .pipe(gulp.dest('dist'));
+        .pipe(rename(function (path) {
+            path.extname = '.min.css'; //文件名.min.css
+        }))
+        .pipe(gulp.dest('dist')); //编译后输出目录
 });
 
 /**
- * 当less文件发生改变时，重新编译less
+ * 当less文件发生改变时，重新编译less，命令：gulp lessWatch
  */
 gulp.task('lessWatch', function () {
     gulp.watch('src/*.less', ['less']);
